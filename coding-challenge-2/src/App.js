23f2f3911c4872274d88825c4cb1fb5f8675e16a
Dohee Lee <dojuhee2015@gmail.com>
2023-09-12 20:48:57 +0100

    add some changes

--- coding-challenge-2/src/App.js
+++ coding-challenge-2/src/App.js
@@ -2,13 +2,17 @@ import './App.css'
 import JobFeed from './Jobfeed'
 import JobPost from './JobPost'
 import { Routes, Route } from 'react-router-dom'
+import Counter from './Counter'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<JobFeed />}></Route>
-      <Route path='/:jobPostId' element={<JobPost />}></Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<JobFeed />}></Route>
+        <Route path='/:jobPostId' element={<JobPost />}></Route>
+      </Routes>
+      <Counter label={'Wow'} />
+    </>
   )
 }
 
